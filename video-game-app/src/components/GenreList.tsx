0209aca4fd79245a504a141baa2b4f8a0eb1515d
sivakumar <siva.kumar@bigsteptech.com>
2023-12-16 12:30:38 +0530

    Creating a generic data fetching hook

--- video-game-app/src/components/GenreList.tsx
+++ video-game-app/src/components/GenreList.tsx
@@ -1,12 +1,11 @@
+// import useData from '../hooks/useData'
 import useGenres from '../hooks/useGenres'
 
 const GenreList = () => {
-    const { genres } = useGenres()
-    console.log(genres);
-
+    const { data } = useGenres();
     return (
         <ul>
-            {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
         </ul>
     )
 }
---
+++ video-game-app/src/hooks/useGenres.ts
@@ -0,0 +1,10 @@
+import useData from "./useData";
+
+interface Genre {
+    id: number
+    name: string
+}
+
+
+const useGenres = () => useData<Genre>('/genres')
+export default useGenres;
