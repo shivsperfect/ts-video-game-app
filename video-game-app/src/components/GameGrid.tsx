3f1dee5def5a93b25e17053b375c62a31e972c6d
shivsperfect <shivaaero10@gmail.com>
2023-12-16 12:30:38 +0530

    Creating a generic data fetching hook

--- video-game-app/src/components/GameGrid.tsx
+++ video-game-app/src/components/GameGrid.tsx
@@ -5,18 +5,18 @@ import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
-    const { games, error, isLoading } = useGames();
+    const { data, error, isLoading } = useGames();
     const skeletons = [1, 2, 3, 4, 5, 6];
-    console.log('isLoading', isLoading);
+
     return (
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" spacing={10}>
-                {isLoading && skeletons.map(skeleton => <GameCardContainer>
-                    <GameCardSkeleton key={skeleton} />
+                {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}>
+                    <GameCardSkeleton />
                 </GameCardContainer>)}
-                {games.map(game => <GameCardContainer>
-                    <GameCard key={game.id} game={game} />
+                {data.map(game => <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
                 </GameCardContainer>)}
             </SimpleGrid>
         </>
--- video-game-app/src/components/PlatformIconsList.tsx
+++ video-game-app/src/components/PlatformIconsList.tsx
@@ -24,7 +24,7 @@ const PlatformIconsList = ({ platforms }: Props) => {
   }
   return (
     <HStack marginY={1}>
-      {platforms.map(platform => <Icon as={iconMap[platform.slug]} color="gray.500">{platform.name}</Icon>)}
+      {platforms.map(platform => <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500">{platform.name}</Icon>)}
     </HStack>
   )
 }
