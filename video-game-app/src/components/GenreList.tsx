0f2dceafb07ffb028e9fe31475c6c3c7f1818c33
sivakumar <siva.kumar@bigsteptech.com>
2023-12-16 10:54:56 +0530

    Fetching the genres

---
+++ video-game-app/src/components/GenreList.tsx
@@ -0,0 +1,14 @@
+import useGenres from '../hooks/useGenres'
+
+const GenreList = () => {
+    const { genres } = useGenres()
+    console.log(genres);
+
+    return (
+        <ul>
+            {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+        </ul>
+    )
+}
+
+export default GenreList
