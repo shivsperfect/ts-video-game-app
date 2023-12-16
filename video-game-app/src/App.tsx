d89432a794f23aebdeba1c6f1cd5fffe05387e99
shivsperfect <shivaaero10@gmail.com>
2023-12-16 18:14:37 +0530

    Filter Games by Genre

--- video-game-app/src/App.tsx
+++ video-game-app/src/App.tsx
@@ -2,8 +2,12 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,
@@ -19,10 +23,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList />
+          <GenreList onSelectGenre={genre => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
-      <GridItem area="main"><GameGrid /></GridItem>
+      <GridItem area="main"><GameGrid selectedGenre={selectedGenre} /></GridItem>
     </Grid>
   )
 }
--- video-game-app/src/components/GameGrid.tsx
+++ video-game-app/src/components/GameGrid.tsx
@@ -3,10 +3,17 @@ import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
+import { Genre } from '../hooks/useGenres';
 
-const GameGrid = () => {
-    const { data, error, isLoading } = useGames();
-    const skeletons = [1, 2, 3, 4, 5, 6];
+interface Props {
+    selectedGenre: Genre | null
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+    const { data, error, isLoading } = useGames(selectedGenre);
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    console.log('isLoading in gameGrid', isLoading)
 
     return (
         <>
@@ -15,7 +22,7 @@ const GameGrid = () => {
                 {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}>
                     <GameCardSkeleton />
                 </GameCardContainer>)}
-                {data.map(game => <GameCardContainer key={game.id}>
+                {!isLoading && data.map(game => <GameCardContainer key={game.id}>
                     <GameCard game={game} />
                 </GameCardContainer>)}
             </SimpleGrid>
