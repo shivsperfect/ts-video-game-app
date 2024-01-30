4a89531a9fa99f05df2bbb59a35c03694b8c98f0
shivsperfect <shivaaero10@gmail.com>
2024-01-30 05:49:44 +0530

    ship static data for genre and platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,5 @@
-import useData from "./useData"
+import platform from "../data/platform"
 
-interface Platform {
-    id: number
-    name: string
-    slug: string
-}
-
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => ({data: platform, isLoading: false, error: false })
 
 export default usePlatforms;
